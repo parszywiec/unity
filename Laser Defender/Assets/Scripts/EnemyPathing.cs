fe637455e111cba2ccf71c6d22547659faa7c4be
parszywiec <bbboguszewski@gmail.com>
2018-10-16 16:19:21 +0200

    enemies spawnuja sie, leca po sciezce, gina na jej koncu - 2 rozne sciezki, 2 rozne statki wrogow

---
+++ Laser Defender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+
+    WaveConfig waveConfig;
+    List<Transform> waypoints;
+    int waypointIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+    }
+
+    public void SetWaveConfig(WaveConfig waveConfig)
+    {
+        this.waveConfig = waveConfig; // odwrotnie niz w javie?
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = Time.deltaTime * waveConfig.GetMoveSpeed();
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition) waypointIndex++;
+        }
+        else Destroy(gameObject);
+    }
+}
---
+++ Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] List<WaveConfig> waveConfigs;
+    [SerializeField] int startingWave = 0;
+    [SerializeField] bool looping = false;
+
+	// Use this for initialization
+	IEnumerator Start ()
+    {
+        do
+        {
+            yield return StartCoroutine(SpawnAllWaves());
+        } while (looping);
+	}
+
+    private IEnumerator SpawnAllWaves()
+    {
+        for (int wavesIndex = 0; wavesIndex < waveConfigs.Count; wavesIndex++)
+        {
+            var currentWave = waveConfigs[wavesIndex];
+            yield return StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+
+        }
+    }
+	
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        for (int enemyCount = startingWave; enemyCount < waveConfig.GetNumberOfEnemies(); enemyCount++)
+        {
+            var newEnemy = Instantiate(
+                                waveConfig.GetEnemyPrefab(),
+                                waveConfig.GetWaypoints()[0].transform.position,
+                                Quaternion.identity); // <- oznacza to, zacznij roatcje od nowa - w skrocie 
+            newEnemy.GetComponent<EnemyPathing>().SetWaveConfig(waveConfig);
+            yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+        }
+    }
+}
