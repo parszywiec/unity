717a30bc7df1d68e87fe16ba47b07800e119b5fd
parszywiec <bbboguszewski@gmail.com>
2018-10-28 10:50:27 +0100

    na 2810

---
+++ Glitch Garden/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    [SerializeField] float delayInSeconds = 5f;
+    int currentSceneIndex;
+
+    public void Start()
+    {
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneIndex == 0) StartCoroutine(WaitAndLoadNextScene());
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    private IEnumerator WaitAndLoadNextScene()
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        LoadNextScene();
+    }
+}
