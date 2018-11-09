89a9df911e182d939c5e4764569e794507379581
parszywiec <bbboguszewski@gmail.com>
2018-11-09 15:46:54 +0100

    animacje w glitch garden lizard, trophy, cactus

---
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour {
+
+    [Range(0f,5f)][SerializeField] float currentSpeed = 0f;
+
+
+	void Update () {
+        // metoda do proszuszania obiektem, od razu ze wskaznikiem lewo/prawo/gora/dol itd.
+        transform.Translate(Vector2.left * Time.deltaTime /*frame rate independent*/  *  currentSpeed);
+	}
+
+    public void setMovementSpeed(float speed)
+    {
+        currentSpeed = speed;
+    }
+
+}
---
+++ Glitch Garden/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour {
+
+    bool spawn = true;
+    [SerializeField] Attacker Lizard;
+    [SerializeField] float minSpawnDelay = 1f, maxSpawnDealy = 5f;
+
+
+	IEnumerator Start () {
+        while(spawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDealy));
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(Lizard, transform.position, transform.rotation);
+    }
+}
