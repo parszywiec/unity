23f8b725b76fec159164fdbd4bfc36bce8c95300
bboguszewski <b_boguszewski@o2.pl>
2018-11-12 23:34:38 +0100

    nowy kurs - roll a zombie zrobione

---
+++ Roll_A_Zombie_demo/Assets/Scripts/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public GameManager gameManager;
+    public AudioClip hit;
+    AudioSource source;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameManager.AddScore();
+        source.PlayOneShot(hit);
+    }
+}
