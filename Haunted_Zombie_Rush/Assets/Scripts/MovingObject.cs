08dd359cbe34732abca870bd0cdb8a06757d35ba
parszywiec <bbboguszewski@gmail.com>
2018-11-13 16:10:14 +0100

    haunted endless runner in progress

---
+++ Haunted_Zombie_Rush/Assets/Scripts/MovingObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour {
+
+    [SerializeField] private float objectSpeed = 1f;
+    [SerializeField] private float resetPostion = 0f;
+    [SerializeField] private float startPostion = 0f;
+
+    void Update () {
+		
+	}
+
+    private void MoveLeft()
+    {
+
+    }
+}
